82c58e9d326c761b994edcaf895d912a06772653
Vu Nguyen <65920518+vanguyendev@users.noreply.github.com>
2021-09-25 23:50:41 -0400

    Update settings.py

--- nguyendev/settings.py
+++ nguyendev/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img').replace('\\', '/')
 MEDIA_URL = '/img/'
